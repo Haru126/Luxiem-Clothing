9b4cbffb85c791481e9bc9adcef7f85b6260103c
Haru126 <aezmedina3@gmail.com>
2024-09-28 16:04:48 +0800

    changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,65 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = [
+        { trigger: 'fa-user', dropdown: 'userdropdown', display: 'block' },
+        { trigger: 'fa-cart-shopping', dropdown: 'cartdropdown', display: 'block' },
+        { trigger: 'fa-headset', dropdown: 'servicedropdown', display: 'inline-flex' }
+    ];
 
+    elements.forEach(({ trigger, dropdown, display }) => {
+        const triggerEl = document.getElementsByClassName(trigger)[0];
+        const dropdownEl = document.getElementsByClassName(dropdown)[0];
+        
+        const showDropdown = () => { dropdownEl.style.display = display; };
+        const hideDropdown = () => { dropdownEl.style.display = "none"; };
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    const user = document.getElementsByClassName('fa-user')[0];
-    const userdd = document.getElementsByClassName('userdropdown')[0];
-    const cart = document.getElementsByClassName('fa-cart-shopping')[0];
-    const cartdd = document.getElementsByClassName('cartdropdown')[0];
-    const service = document.getElementsByClassName('fa-headset')[0];
-    const servicedd = document.getElementsByClassName('servicedropdown')[0];
-
-
-    user.addEventListener("mouseover", function() {
-        document.getElementById('userdropdown').style.display = "block";
-        console.log("show");
-    })
-
-    user.addEventListener("mouseout", function() {
-        document.getElementById('userdropdown').style.display = "none";
-    })
-
-    userdd.addEventListener("mouseover", function() {
-        document.getElementById('userdropdown').style.display = "block";
-        console.log("show");
-    })
-
-    userdd.addEventListener("mouseout", function() {
-        document.getElementById('userdropdown').style.display = "none";
-    })
-
-    cart.addEventListener("mouseover", function() {
-        document.getElementById('cartdropdown').style.display = "block";
-        console.log("show");
-    })
-
-    cart.addEventListener("mouseout", function() {
-        document.getElementById('cartdropdown').style.display = "none";
-    })
-
-    cartdd.addEventListener("mouseover", function() {
-        document.getElementById('cartdropdown').style.display = "block";
-        console.log("show");
-    })
-
-    cartdd.addEventListener("mouseout", function() {
-        document.getElementById('cartdropdown').style.display = "none";
-    })
-
-    service.addEventListener("mouseover", function() {
-        document.getElementById('servicedropdown').style.display = "inline-flex";
-        console.log("show");
-    })
-
-    service.addEventListener("mouseout", function() {
-        document.getElementById('servicedropdown').style.display = "none";
-    })
-
-    servicedd.addEventListener("mouseover", function() {
-        document.getElementById('servicedropdown').style.display = "inline-flex";
-        console.log("show");
-    })
-
-    servicedd.addEventListener("mouseout", function() {
-        document.getElementById('servicedropdown').style.display = "none";
-    })
+        triggerEl.addEventListener("mouseover", showDropdown);
+        triggerEl.addEventListener("mouseout", hideDropdown);
+        dropdownEl.addEventListener("mouseover", showDropdown);
+        dropdownEl.addEventListener("mouseout", hideDropdown);
+    });
 });
